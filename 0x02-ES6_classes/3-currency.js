a5981430a088e8de6fb7964210bc7bcaa9d7127f
Gizachew Bayness <eleccrazy24@gmail.com>
2023-01-25 22:06:31 +0300

    task 3

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    this.code = code;
+    this.name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a sting');
+    }
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    console.log(`${this._name} (${this._code})`);
+  }
+}
