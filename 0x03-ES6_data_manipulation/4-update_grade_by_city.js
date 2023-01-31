1dee6b846041f91c2d2007a6b04968a9df203bc1
Gizachew Bayness <eleccrazy24@gmail.com>
2023-01-31 20:29:00 +0300

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,15 @@
-export default function updateStudentGradeByCity(studnets, city, newGrades) {
-  const filteredStudents = studnets.filter(
-    (student) => student.location === city,
-  );
-  return filteredStudents.map((std) => {
-    const targetGrades = newGrades.filter(
-      (grades) => grades.studentId === std.id,
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (students instanceof Array) {
+    const filteredStudents = students.filter(
+      (student) => student.location === city,
     );
-    std.grade = targetGrades[0] ? targetGrades[0].grade : 'NaN'; // eslint-disable-line no-param-reassign
-    return std;
-  });
+    return filteredStudents.map((std) => {
+      const targetGrades = newGrades.filter(
+        (grades) => grades.studentId === std.id,
+      );
+      std.grade = targetGrades[0] ? targetGrades[0].grade : 'NaN'; // eslint-disable-line no-param-reassign
+      return std;
+    });
+  }
+  return [];
 }
