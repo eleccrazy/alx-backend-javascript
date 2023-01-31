a129c463c3f9ab2a60c2c9238b1fb16c74b3a2cc
Gizachew Bayness <eleccrazy24@gmail.com>
2023-01-31 21:12:51 +0300

    task 8

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString) {
+  if (!set || !startString) {
+    return '';
+  }
+  const subString = [];
+  for (const elem of set.values()) {
+    if (elem.startsWith(startString)) {
+      const sub = elem.substring(startString.length);
+      if (sub) {
+        subString.push(sub);
+      }
+    }
+  }
+  return subString.join('-');
+}
