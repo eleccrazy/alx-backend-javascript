8de10cc3183ad01138d26aeee57a418d273aac0d
Gizachew Bayness <eleccrazy24@gmail.com>
2023-01-31 21:24:57 +0300

    Fixed

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,17 @@
 export default function cleanSet(set, startString) {
-  if (!set || !startString) {
+  if (
+    !set
+    || !startString
+    || !(set instanceof Set)
+    || typeof startString !== 'string'
+  ) {
     return '';
   }
   const subString = [];
   for (const elem of set.values()) {
-    if (elem.startsWith(startString)) {
+    if (typeof elem === 'string' && elem.startsWith(startString)) {
       const sub = elem.substring(startString.length);
-      if (sub && sub !== elem) {
+      if (sub) {
         subString.push(sub);
       }
     }
