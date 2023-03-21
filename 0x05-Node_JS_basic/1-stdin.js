cac1494ee313653690671aefde2c6e0f6c7eeae2
Gizachew Bayness <eleccrazy24@gmail.com>
2023-03-21 15:47:01 +0300

    Convert the input data to string while printing

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 console.log('Welcome to Holberton School, what is your name? ');
 process.stdin.on('data', (name) => {
-  process.stdout.write(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name.toString()}`);
   console.log('This important software is now closing');
   process.exit();
 });
